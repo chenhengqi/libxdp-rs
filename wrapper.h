4d209375653412a53508e434266f7f48a64f1871
Hengqi Chen <chenhengqi@outlook.com>
2021-05-01 14:55:06 +0800

    add parsing helpers
    Signed-off-by: Hengqi Chen <chenhengqi@outlook.com>

--- wrapper.h
+++ wrapper.h
@@ -1,8 +1,13 @@
 #include "xdp-tools/lib/libbpf/src/libbpf.h"
+#include "xdp-tools/lib/libbpf/src/bpf_endian.h"
 
 #include "xdp-tools/headers/xdp/xdp_helpers.h"
 #include "xdp-tools/headers/xdp/libxdp.h"
-// #include "xdp-tools/headers/xdp/parsing_helpers.h"
 #include "xdp-tools/headers/xdp/prog_dispatcher.h"
 #include "xdp-tools/headers/xdp/xdp_stats_kern_user.h"
 // #include "xdp-tools/headers/xdp/xdp_stats_kern.h"
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wcompare-distinct-pointer-types"
+#include "xdp-tools/headers/xdp/parsing_helpers.h"
+#pragma clang diagnostic pop
