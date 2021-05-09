a973f6d2e98b27ff4046c4621e8d4214da38138c
Hengqi Chen <chenhengqi@outlook.com>
2021-05-09 10:23:52 +0800

    remove BPF program related headers

--- wrapper.h
+++ wrapper.h
@@ -5,9 +5,3 @@
 #include "xdp-tools/headers/xdp/libxdp.h"
 #include "xdp-tools/headers/xdp/prog_dispatcher.h"
 #include "xdp-tools/headers/xdp/xdp_stats_kern_user.h"
-// #include "xdp-tools/headers/xdp/xdp_stats_kern.h"
-
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wcompare-distinct-pointer-types"
-#include "xdp-tools/headers/xdp/parsing_helpers.h"
-#pragma clang diagnostic pop
